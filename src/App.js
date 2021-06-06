84ac5045d141abbebdc429782d9c8b5278802f6e
César Richard <cesar.richard@weezevent.com>
2021-06-06 15:15:21 +0200

    Cleanup manifests

--- src/App.js
+++ src/App.js
@@ -5,27 +5,30 @@ import OrderCard from "./components/OrderCard";
 
 function App() {
   return (
-    <div className="mainContainer">
-      {[
-        {
-          key: 1,
-          name: "Cesar Richard",
-          order: [{ item: "Chips au poivron", quantity: 1 }]
-        },
-        {
-          key: 2,
-          name: "Louise Mention",
-          order: [{ item: "Chips au poivron", quantity: 1 }]
-        },
-        {
-          key: 3,
-          name: "Alice Mention",
-          order: [{ item: "Chips au poivron", quantity: 1 }]
-        }
-      ].map(x => (
-        <OrderCard key={x.key} name={x.name} order={x.order} />
-      ))}
-    </div>
+    <>
+      <Router></Router>
+      <div className="mainContainer">
+        {[
+          {
+            key: 1,
+            name: "Cesar Richard",
+            order: [{ item: "Chips au poivron", quantity: 1 }]
+          },
+          {
+            key: 2,
+            name: "Louise Mention",
+            order: [{ item: "Chips au poivron", quantity: 1 }]
+          },
+          {
+            key: 3,
+            name: "Alice Mention",
+            order: [{ item: "Chips au poivron", quantity: 1 }]
+          }
+        ].map(x => (
+          <OrderCard key={x.key} name={x.name} order={x.order} />
+        ))}
+      </div>
+    </>
   );
 }
 
